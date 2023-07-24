6e671f8af9c7ace81d9ce56ca00f6fb2219f2d64
nailyk <nailyk-fr@users.noreply.github.com>
2023-07-24 14:03:01 +0200

    backup.sh: Update app list

--- backup.sh
+++ backup.sh
@@ -7,15 +7,19 @@ for app in /data/data/com.fsck.k9 \
   /data/data/org.smssecure.smssecure \
   /data/data/org.mozilla.fennec_fdroid \
   /data/data/org.schabi.newpipe \
-  /data/data/com.github.dfa.diaspora_android \
-  /data/data/com.owncloud.android \
   /data/data/im.vector.alpha \
   /data/data/name.boyle.chris.sgtpuzzles \
   /data/data/com.bil.android \
-  /data/data/com.samsung.android.app.watchmanager \
-  /data/data/com.nextcloud.client \
-  /data/data/org.telegram.messenger \
-  /data/data/de.arnowelzel.android.periodical
+  /data/data/de.arnowelzel.android.periodical \
+  /data/data/org.liberty.android.freeotpplus \
+  /data/data/com.android.deskclock \
+  /data/data/com.keylesspalace.tusky.test \
+  /data/data/com.huami.watch.hmwatchmanager \
+  /data/data/org.schabi.newpipe \
+  /data/data/org.openhab.habdroid \
+  /data/data/lacaveauxenigmes.game.com \
+  /data/data/dev.ukanth.ufirewall \
+  /data/data/lu.post.finance.mobile.eboo
 do
 if [[ -d ${app} ]]
 then
